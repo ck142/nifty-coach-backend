e60347a8dc832a8af5fd0e692cc1956b51419480
ck142 <ckw142@gmail.com>
2025-07-19 23:01:03 -0400

    fix 6

--- routers/trades.py
+++ routers/trades.py
@@ -10,6 +10,7 @@ router = APIRouter()
 def sync_trades():
     try:
         trades = fetch_trades()
+        print("Dhan response JSON:", response.json())
         db = SessionLocal()
         new_trades = 0
         for trade in trades:
